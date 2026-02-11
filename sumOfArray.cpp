f60dfa9f636b630813b441778438c5e7ca606fa3
justnothing-h <harshit.choudhary0715@gmail.com>
2026-02-11 17:51:27 +0530

    updated

---
+++ sumOfArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];
+    int sum = 0;
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << "Sum of elements = " << sum;
+
+    return 0;
+}
