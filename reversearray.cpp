2ce9b788e29071d74255b43ae7fa6adec7f68fcf
justnothing-h <harshit.choudhary0715@gmail.com>
2026-02-11 17:53:44 +0530

    updated

---
+++ reversearray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Reversed array:\n";
+    for(int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
