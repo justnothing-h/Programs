d259caa67d0e2b3128acdd0c6de2f36bc45e2a28
justnothing-h <harshit.choudhary0715@gmail.com>
2026-02-13 21:59:21 +0530

    new

---
+++ removedup.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Array after removing duplicates:\n";
+
+    for(int i = 0; i < n; i++) {
+        bool duplicate = false;
+
+        for(int j = 0; j < i; j++) {
+            if(arr[i] == arr[j]) {
+                duplicate = true;
+                break;
+            }
+        }
+
+        if(!duplicate)
+            cout << arr[i] << " ";
+    }
+
+    return 0;
+}
