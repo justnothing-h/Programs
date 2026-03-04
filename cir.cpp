c9d8350a075af25aaaed4b1e32313c534babae83
justnothing-h <harshit.choudhary0715@gmail.com>
2026-03-04 21:53:06 +0530

    updated

---
+++ cir.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Shape{
+
+public:
+
+virtual void draw()=0;
+
+};
+
+class Circle : public Shape{
+
+public:
+
+void draw(){
+cout<<"Circle drawn";
+}
+
+};
+
+int main(){
+
+Circle c;
+c.draw();
+
+}
