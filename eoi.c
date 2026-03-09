ec58c99b350f0cea34b9e14715cd03dfeee0e4ba
justnothing-h <harshit.choudhary0715@gmail.com>
2026-03-09 14:37:33 +0530

    updated

---
+++ eoi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+enum Level {LOW = 1, MEDIUM, HIGH}; // LOW is 1, MEDIUM is 2...
+
+int main() {
+    enum Level myVar = MEDIUM;
+    printf("Selected Level: %d\n", myVar);
+
+    if(myVar == 2) {
+        printf("Processing Medium Priority Task...");
+    }
+    return 0;
+}
